f6408b714d6decb00d7d9211b82ffd2b93632c0d
samandar_developer1 <90138640+samandar-shoyimov@users.noreply.github.com>
2022-02-22 22:38:15 +0500

    added crud

--- base/views.py
+++ base/views.py
@@ -94,32 +94,42 @@ def userProfile(request, pk):
 
 @login_required(login_url='login')
 def createRoom(request):
-  if request.method  == 'POST':
-    form = RoomForm(request.POST)
-    if form.is_valid():
-      room = form.save(commit=False)
-      room.host = request.user
-      room.save()
-      return redirect('/')
+  topics = Topic.objects.all()
+  
+  if request.method == 'POST':
+    topic_name = request.POST['topic']
+    topic, created = Topic.objects.get_or_create(name=topic_name)
+    Room.objects.create(
+      host = request.user,
+      topic = topic,
+      name = request.POST['name'],
+      description = request.POST['description'],
+    )
+    return redirect('/')
   form = RoomForm()
-  context = {'form':form}
+  context = {'form':form, 'topics':topics}
   return render(request, 'base/room_form.html', context)
-
 
 @login_required(login_url='login')
 def updateRoom(request, pk):
-  room = Room.objects.get(id=pk)
-  form = RoomForm(instance=room)
-  
-  if request.user != room.host:
-    return HttpResponse(f'You are not user of "{room.name}"')
-  if request.method == 'POST':
-    form = RoomForm(request.POST, instance=room)
-    if form.save():
-      form.save()
-      return redirect('/')
-  context = {'room':room, 'form':form}
-  return render(request, 'base/room_form.html', context)
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    topics = Topic.objects.all()
+    if request.user != room.host:
+        return HttpResponse('Your are not allowed here!!')
+
+    if request.method == 'POST':
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
+
+    context = {'form': form, 'topics': topics, 'room': room}
+    return render(request, 'base/room_form.html', context)
+
 
 @login_required(login_url='login')
 def deleteRoom(request, pk):
